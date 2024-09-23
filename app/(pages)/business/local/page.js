c209993daa8d4e10d9d42803bb31dd93175e97d6
Rasmus Jensing <rasmus@happenings.dk>
2024-09-23 19:06:07 +0200

    add how_it_works

--- app/(pages)/business/local/page.js
+++ app/(pages)/business/local/page.js
@@ -1,5 +1,6 @@
 import BenefitsLocal from "@/app/(components)/pages/business/local/benefits";
 import HeroLocal from "@/app/(components)/pages/business/local/hero";
+import HowItWorksLocal from "@/app/(components)/pages/business/local/how_it_works";
 import IncentivesLocal from "@/app/(components)/pages/business/local/incentives";
 import LocalPricing from "@/app/(components)/pages/business/local/pricing";
 import Footer from "@/app/(components)/universal/navigation/footer/footer";
@@ -8,6 +9,7 @@ export default function Business() {
   return (
     <main>
       <HeroLocal />
+      <HowItWorksLocal />
       <IncentivesLocal />
       <LocalPricing />
       <BenefitsLocal />
